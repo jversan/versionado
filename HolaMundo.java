6f4a8a9b08fb840dc5053bca368d9fee2f1a7535
jversan <jversan516@iesalmudeyne.es>
2023-11-09 14:13:33 +0100

    testeando el etiquetado de versiones

---
+++ HolaMundo.java
@@ -0,0 +1,8 @@
+/**
+ * Muestra por pantalla la frase "Hola Mundo"
+ */
+public class HolaMundo {
+    public static void main (Strinf[] args) {
+        System.out.println("¡Hola Mundo!");
+    }
+}
