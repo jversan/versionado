d07ea3d90da8f18ed05d99dabdd56a36baf0fdcc
jversan <jversan516@iesalmudeyne.es>
2023-11-09 14:15:55 +0100

    Seguimos testeando el etiquetado de versiones

--- HolaMundo.java
+++ HolaMundo.java
@@ -4,5 +4,6 @@
 public class HolaMundo {
     public static void main (Strinf[] args) {
         System.out.println("¡Hola Mundo!");
+        System.out.println("¡Aquí estamos haciendo pruebas en Git");
     }
 }
